6a3a5e2027a5870019b8684bd095b7494dbcc8d9
Benjamin Lindley <benjameslindley@gmail.com>
2023-07-27 22:11:03 -0500

    move content of stb.cpp to image.cpp

--- src/image.cpp
+++ src/image.cpp
@@ -3,7 +3,11 @@
 #include <cassert>
 #include <sstream>
 
+#define STBI_FAILURE_USERMSG
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
 #include "declarations.h"
